0cd873c3692c609faa763d061a403f09ef330209
Rushikesh <rushibhosalepro@gmail.com>
2025-10-16 17:04:46 +0530

    adding tsconfig no check

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   port: process.env.PORT!,
   clearkSignIn: process.env.CLERK_SIGN_IN_URL!,
-  heliusApiKey: process.env.HELIUS_API_KEY!,
+
   webhookUrl: "https://51892ba4da03.ngrok-free.app/webhook/helius",
 };
